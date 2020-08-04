ebd4749d354e6d754776b0d67d8893858aab14a0
leqi <michaelleqihust@gmail.com>
2020-08-04 21:04:24 +0800

    [leqi] feat: Finish changing RsEvent from string to class.

--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -7,9 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 
 @SpringBootTest
@@ -17,36 +18,76 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class RsControllerTest {
     final String GET_ONE_RS_EVENT_URL = "/rs/%d";
     final String GET_MULTIPLE_RS_EVENT_URL = "/rs/list?start=%d&end=%d";
+    final String POST_ONE_RS_EVENT_URL = "/rs";
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
     public void should_get_one_rs_event_by_index() throws Exception {
-        mockMvc.perform(get(String.format(GET_ONE_RS_EVENT_URL, 0)).accept(MediaType.TEXT_PLAIN))
-                .andExpect(content().string("第一条事件"))
+        mockMvc.perform(get(String.format(GET_ONE_RS_EVENT_URL, 0)).accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8"))
+                .andExpect(jsonPath("$.eventName", is("第一条事件")))
+                .andExpect(jsonPath("$.keyword", is("分类一")))
                 .andExpect(status().isOk());
-        mockMvc.perform(get(String.format(GET_ONE_RS_EVENT_URL, 1)).accept(MediaType.TEXT_PLAIN))
-                .andExpect(content().string("第二条事件"))
+        mockMvc.perform(get(String.format(GET_ONE_RS_EVENT_URL, 1)).accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8"))
+                .andExpect(jsonPath("$.eventName", is("第二条事件")))
+                .andExpect(jsonPath("$.keyword", is("分类二")))
                 .andExpect(status().isOk());
-        mockMvc.perform(get(String.format(GET_ONE_RS_EVENT_URL, 2)).accept(MediaType.TEXT_PLAIN))
-                .andExpect(content().string("第三条事件"))
+        mockMvc.perform(get(String.format(GET_ONE_RS_EVENT_URL, 2)).accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8"))
+                .andExpect(jsonPath("$.eventName", is("第三条事件")))
+                .andExpect(jsonPath("$.keyword", is("分类三")))
                 .andExpect(status().isOk());
     }
 
     @Test
     public void should_get_multiple_rs_event_by_indexes() throws Exception {
-        mockMvc.perform(get(String.format(GET_MULTIPLE_RS_EVENT_URL, 0, 1)).accept(MediaType.TEXT_PLAIN))
+        mockMvc.perform(get(String.format(GET_MULTIPLE_RS_EVENT_URL, 0, 1)).accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8"))
+                .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
+                .andExpect(jsonPath("$[0].keyword", is("分类一")))
+                .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
+                .andExpect(jsonPath("$[1].keyword", is("分类二")))
+                .andExpect(status().isOk());
+        mockMvc.perform(get(String.format(GET_MULTIPLE_RS_EVENT_URL, 1, 2)).accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8"))
+                .andExpect(jsonPath("$[0].eventName", is("第二条事件")))
+                .andExpect(jsonPath("$[0].keyword", is("分类二")))
+                .andExpect(jsonPath("$[1].eventName", is("第三条事件")))
+                .andExpect(jsonPath("$[1].keyword", is("分类三")))
+                .andExpect(status().isOk());
+        mockMvc.perform(get(String.format(GET_MULTIPLE_RS_EVENT_URL, 0, 2)).accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8"))
+                .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
+                .andExpect(jsonPath("$[0].keyword", is("分类一")))
+                .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
+                .andExpect(jsonPath("$[1].keyword", is("分类二")))
+                .andExpect(jsonPath("$[2].eventName", is("第三条事件")))
+                .andExpect(jsonPath("$[2].keyword", is("分类三")))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list").accept(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8"))
+                .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
+                .andExpect(jsonPath("$[0].keyword", is("分类一")))
+                .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
+                .andExpect(jsonPath("$[1].keyword", is("分类二")))
+                .andExpect(jsonPath("$[2].eventName", is("第三条事件")))
+                .andExpect(jsonPath("$[2].keyword", is("分类三")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void should_add_one_rs_event_by_json() throws Exception {
+        /*mockMvc.perform(post(POST_ONE_RS_EVENT_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content())
                 .andExpect(content().string("[第一条事件, 第二条事件]"))
                 .andExpect(status().isOk());
-        mockMvc.perform(get(String.format(GET_MULTIPLE_RS_EVENT_URL, 1, 2)).accept(MediaType.TEXT_PLAIN))
-                .andExpect(content().string("[第二条事件, 第三条事件]"))
-                .andExpect(status().isOk());
-        mockMvc.perform(get(String.format(GET_MULTIPLE_RS_EVENT_URL, 0, 2)).accept(MediaType.TEXT_PLAIN))
-                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"))
-                .andExpect(status().isOk());
-        mockMvc.perform(get("/rs/list").accept(MediaType.TEXT_PLAIN))
-                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"))
-                .andExpect(status().isOk());
+
+        mockMvc.perform(get(String.format(GET_ONE_RS_EVENT_URL, 3)).accept(MediaType.TEXT_PLAIN))
+                .andExpect(content().string("第四条事件"))
+                .andExpect(status().isOk());*/
     }
 }
