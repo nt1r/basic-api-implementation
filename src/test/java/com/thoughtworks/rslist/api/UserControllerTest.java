76db6ede1808738f5bb8e6389db1c49a5979e26d
leqi <michaelleqihust@gmail.com>
2020-08-05 15:10:29 +0800

    [leqi] feat: should not create user with invalid name: 1. username over 8 characters. 2. username null.

--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -27,6 +27,8 @@ class UserControllerTest {
     final String ADD_USER_URL = "/user";
 
     final User userDwight = new User("Dwight", 25, "male", "michaelleqihust@gmail.com", "18706789189");
+    final User userNameTooLong = new User("DwightClaudette", 25, "male", "michaelleqihust@gmail.com", "18706789189");
+    final User userNameNull = new User(null, 25, "male", "michaelleqihust@gmail.com", "18706789189");
 
     @BeforeEach
     void setUp() {
@@ -62,5 +64,23 @@ class UserControllerTest {
         int userCountAfterCreateSameUser = UserController.userList.size();
 
         assertEquals(userCountBeforeCreateSameUser, userCountAfterCreateSameUser);
+    }
+
+    @Test
+    void should_not_create_user_with_name_over_8_characters() throws Exception {
+        mockMvc.perform(post(ADD_USER_URL)
+                .characterEncoding("UTF-8")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(userNameTooLong)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void should_not_create_user_with_name_null() throws Exception {
+        mockMvc.perform(post(ADD_USER_URL)
+                .characterEncoding("UTF-8")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(userNameNull)))
+                .andExpect(status().isBadRequest());
     }
 }
